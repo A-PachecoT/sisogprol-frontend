9d73a891dffbbe6ae55e80aa7484304ce058ab0d
thsergitox <sergio.pezo.j@uni.pe>
2024-06-12 19:00:59 -0500

    feat: added pedido interface

---
+++ src/app/model/pedido.ts
@@ -0,0 +1,10 @@
+// models/pedido.model.ts
+export interface Pedido {
+  id: number;
+  estado: string;
+  descripcion: string;
+  name: string;
+  id_empleado: number;
+  fechacreacion: Date;
+  updated_at: Date;
+}
