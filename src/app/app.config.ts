f139eadb77b4b102009849ddf95baf12ab7654c5
André Pacheco <apachecotaboada@gmail.com>
2024-06-13 13:25:20 -0500

    feat: provideHttpClient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,14 @@ import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),provideClientHydration(),]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideClientHydration(),
+    provideHttpClient(),
+  ]
 };
